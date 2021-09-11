38611038be079e6a41f3ad9db40fb43679ce5f1c
Aadesh-singh <aadeshsingh146@gmail.com>
2021-09-11 09:34:16 +0530

    App Complete By Aadesh Singh

--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ router.get('/', homeController.home);
 
 router.post('/create-task', homeController.createTask);
 
+router.get('/delete-task', homeController.deleteTask);
+
+router.get('/edit-task', homeController.editTask);
+
+router.post('/update-task', homeController.updateTask);
+
 module.exports = router;
